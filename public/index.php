e93e4925558af832781a2ffaaec7125ceb6e1aee
alinidhal <ali.nidhal@hotmail.com>
2021-05-17 16:38:46 +0200

    Mise à jour de post

--- public/index.php
+++ public/index.php
@@ -2,31 +2,14 @@
 
 use Entity\Post;
 use Entity\User;
+use ludk\Persistence\ORM;
 
-require '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+session_start();
+$orm = new ORM(__DIR__ . '/../Resources');
+$postRepo = $orm->getRepository(Post::class);
+$posts = $postRepo->findAll();
 
-$user1 = new User();
-$user1->id = 1;
-$user1->nickname = "user1";
-$user1->paswd = "pswd1";
-
-$post1 = new Post();
-$post1->user_id = 1;
-$post1->title = "Tutu";
-$post1->datetime = "12/12/2021";
-$post1->description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos beatae magni nulla tempora, eum nesciunt sequi quod saepe earum atque optio. Dignissimos iure nemo perspiciatis est aut. Libero, blanditiis ipsa! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, amet eum dolorum, obcaecati illum temporibus perferendis a atque sed dolorem facilis non officiis numquam eaque quas vel. Quia, corporis nobis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis a ipsam illum accusamus molestias quasi suscipit facilis impedit non, in ex maiores obcaecati, officia illo, soluta nam! Incidunt, laudantium consequatur.";
-$post1->url_img = "https://images.pexels.com/photos/2179483/pexels-photo-2179483.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-$post1->user = $user1;
-
-$post2 = new Post();
-$post2->user_id = 2;
-$post2->title = "Toto";
-$post2->datetime = "13/12/2021";
-$post2->description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos beatae magni nulla tempora, eum nesciunt sequi quod saepe earum atque optio. Dignissimos iure nemo perspiciatis est aut. Libero, blanditiis ipsa! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, amet eum dolorum, obcaecati illum temporibus perferendis a atque sed dolorem facilis non officiis numquam eaque quas vel. Quia, corporis nobis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis a ipsam illum accusamus molestias quasi suscipit facilis impedit non, in ex maiores obcaecati, officia illo, soluta nam! Incidunt, laudantium consequatur.";
-$post2->url_img = "https://images.pexels.com/photos/5878801/pexels-photo-5878801.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-$post2->user = $user1;
-
-$items = array($post1, $post2);
 ?>
 
 <!doctype html>
@@ -45,38 +28,39 @@ $items = array($post1, $post2);
 </head>
 
 <body>
-    <nav class="navbar navbar-light fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
         <div class="container">
-            <a class="navbar-brand text-light">MyArt</a>
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
+            <a class="navbar-brand text-light" href="/">MyArt</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <form class="form-inline ml-auto my-2 my-lg-0" action="/">
+                    <input class="form-control mr-sm-2" type="text" name="search" placeholder="Recherche" aria-label="Search">
+                </form>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Connexion</button>
+                <button class="btn btn-success my-2 my-sm-0 ml-2" type="submit">S'inscrire</button>
+            </div>
+        </div>
     </nav>
     <main class="container">
         <div>
             <div class="row">
                 <div class="col-8">
 
-                    <?php {
+                    <?php
+                    foreach ($posts as $post1) {
+
                     ?>
                         <div class="card mb-5">
                             <div class="card-body">
-                                <h3 class="h5 font-weight-bolder"><?= $post1->title ?></h3>
+                                <h3 class="h4 font-weight-bolder"><?= $post1->nickname ?></h3>
+                                <h3 class="h5 "><?= $post1->title ?></h3>
                                 <p class="card-text"><?= $post1->description ?></p>
                                 <p><?= $post1->datetime ?></p>
                             </div>
                             <img src=<?= $post1->url_img ?> class="card-img-top" alt="...">
-                        </div>
-                        <div class="card mb-5">
-                            <div class="card-body">
-                                <h3 class="h5 font-weight-bolder"><?= $post2->title ?></h3>
-                                <p class="card-text"><?= $post2->description ?></p>
-                                <p><?= $post2->datetime ?></p>
-                            </div>
-                            <img src=<?= $post2->url_img ?> class="card-img-top" alt="...">
                         </div>
                     <?php
                     }
@@ -84,42 +68,23 @@ $items = array($post1, $post2);
 
                 </div>
                 <div class="col-4 border-left">
-                    <p>Profil d'utilsateur</p>
-                    <div class="card mt-3">
-                        <img src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947_960_720.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">#Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h2 class="h4">Profil d'utilisateur</h2>
+                    <?php
+                    foreach ($posts as $post1) {
+
+                    ?>
+                        <div class="card mt-2">
+                            <div class="card-body">
+                                <h3 class="h5 font-weight-bolder"><?= $post1->nickname ?></h3>
+                                <h3 class="h6 "><?= $post1->title ?></h3>
+                                <p class="card-text"><?= $post1->description ?></p>
+                                <p><?= $post1->datetime ?></p>
+                            </div>
+                            <img src=<?= $post1->url_img ?> class="card-img-top" alt="...">
                         </div>
-                    </div>
-                    <div class="card mt-3">
-                        <img src="https://cdn.pixabay.com/photo/2017/07/03/20/17/abstract-2468874__340.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                    <div class="card mt-3">
-                        <img src="https://cdn.pixabay.com/photo/2018/02/06/22/43/painting-3135875__340.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                    <div class="card mt-3">
-                        <img src="https://cdn.pixabay.com/photo/2017/10/31/07/49/horses-2904536__340.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                    <div class="card mt-3">
-                        <img src="https://cdn.pixabay.com/photo/2017/05/25/09/22/flower-2342706__340.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
+                    <?php
+                    }
+                    ?>
                 </div>
             </div>
 
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,12 +2,16 @@
 
 namespace Entity;
 
+use ludk\Utils\Serializer;
+
 class Post
 {
-    public $user_id;
+    public $id;
+    public $nickname;
     public $title;
     public $description;
     public $datetime;
     public $url_img;
     public User $user;
+    use Serializer;
 }
